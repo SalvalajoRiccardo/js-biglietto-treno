33d118ece4e9511edb3248700a122a25a7d49fd9
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-05-27 15:59:57 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ console.log(kilometraggio);
 
 var prezzo = kilometraggio * 0.21;
 
+if (age < 18) {
+    prezzo = prezzo - (prezzo / 100 * 20) ;
+} else if (age > 64) { 
+    prezzo = prezzo - (prezzo / 100 * 40) ;
+} 
+
+document.getElementById('prezzo-finale').innerHTML = prezzo.toFixed(2) + "€" ;
+console.log(prezzo);
