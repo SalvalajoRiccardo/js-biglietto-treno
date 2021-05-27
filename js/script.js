30b2665be92df08ed68d7ca634d704f55a2bc7f2
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-05-27 15:37:55 +0200

    add prombt e prezzo base

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var age = parseInt(prompt('quanti anni hai?'))
+console.log(age);
+
+var kilometraggio = (prompt('quanti chilometri devi percorrere?'))
+console.log(kilometraggio);
+
+var prezzo = kilometraggio * 0.21;
+
